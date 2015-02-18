bdb45a2edc5cc1109a5ed543387cb087231acdff
hasherezade <hasherezade@op.pl>
2015-02-18 12:35:01 +0100

    [REFACT] In win_types.h : use stdint.h

--- parser/win_hdrs/win_types.h
+++ parser/win_hdrs/win_types.h
@@ -9,16 +9,9 @@
 #endif
 
 #ifdef _MSC_VER
-    typedef unsigned __int8 uint8_t;
-    typedef unsigned __int16 uint16_t;
-    typedef unsigned __int32 uint32_t;
-    typedef unsigned __int64 uint64_t;
-    typedef __int8 int8_t;
-    typedef __int16 int16_t;
-    typedef __int32 int32_t;
-    typedef __int64 int64_t;
+    #include <stdint.h>
 #else
-# include <inttypes.h>
+    #include <inttypes.h>
 #endif
 
 #ifndef BYTE
