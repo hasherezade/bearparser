c1040427d759aa0bd094f47e88c33bb62468d7e0
hasherezade <hasherezade@op.pl>
2015-03-01 00:26:57 +0100

    [FEATURE] In PEFile : added wrapper function for getImports

--- parser/pe/PEFile.h
+++ parser/pe/PEFile.h
@@ -98,6 +98,12 @@ public:
         return (this->album == NULL) ? NULL : album->getResourcesOfType(typeId);
     }
     DataDirEntryWrapper* getDataDirEntry(pe::dir_entry eType);
+
+    ImportDirWrapper* getImports()
+    {
+        return dynamic_cast<ImportDirWrapper*>(getWrapper(PEFile::WR_DIR_ENTRY + pe::DIR_IMPORT));
+    }
+
     BufferView* createSectionView(size_t secNum);
     //---
     //modifications:
