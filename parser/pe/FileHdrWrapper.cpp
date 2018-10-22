dcadf02c06af321f5ae8e226e78670a2e692881f
hasherezade <hasherezade@gmail.com>
2018-10-22 22:26:44 +0200

    [FEATURE] In FileHdr: translate timestamp to human readable form

--- parser/pe/FileHdrWrapper.cpp
+++ parser/pe/FileHdrWrapper.cpp
@@ -1,10 +1,21 @@
 #include "FileHdrWrapper.h"
 #include "PEFile.h"
 
+#include <time.h>
+#include <QDateTime>
+
+QString getDateString(const quint64 timestamp)
+{
+    const time_t rawtime = (const time_t)timestamp;
+    QString format = "dddd, dd.MM.yyyy hh:mm:ss";
+    QDateTime date1(QDateTime(QDateTime::fromTime_t(rawtime)));
+    return date1.toUTC().toString(format) + " UTC";
+}
+
 void* FileHdrWrapper::getPtr()
 {
-    if (this->hdr != NULL) {
-        // use cached
+    if (this->hdr) {
+        // use cached if exists
         return (void*) hdr;
     }
     if (m_PE == NULL) return NULL;
@@ -19,11 +30,10 @@ void* FileHdrWrapper::getPtr()
 
 void* FileHdrWrapper::getFieldPtr(size_t fieldId, size_t subField)
 {
-    IMAGE_FILE_HEADER * hdr = (IMAGE_FILE_HEADER *) getPtr();
-    if (hdr == NULL) return NULL;
+    IMAGE_FILE_HEADER * hdr = reinterpret_cast<IMAGE_FILE_HEADER*>(getPtr());
+    if (!hdr) return nullptr;
 
     IMAGE_FILE_HEADER &fileHeader = (*hdr);
-
     switch (fieldId) {
          case MACHINE: return (void*) &fileHeader.Machine;
          case SEC_NUM: return (void*) &fileHeader.NumberOfSections;
@@ -53,5 +63,17 @@ QString FileHdrWrapper::getFieldName(size_t fieldId)
 Executable::addr_type FileHdrWrapper::containsAddrType(size_t fieldId, size_t subField)
 {
     return Executable::NOT_ADDR;
+}
+
+QString FileHdrWrapper::translateFieldContent(size_t fieldId)
+{
+    IMAGE_FILE_HEADER * hdr = reinterpret_cast<IMAGE_FILE_HEADER*>(getPtr());
+    if (!hdr) return "";
+
+    IMAGE_FILE_HEADER &fileHeader = (*hdr);
+    switch (fieldId) {
+         case TIMESTAMP: return getDateString(fileHeader.TimeDateStamp);
+    }
+    return "";
 }
 
--- parser/pe/FileHdrWrapper.h
+++ parser/pe/FileHdrWrapper.h
@@ -33,6 +33,8 @@ public:
     virtual void* getFieldPtr(size_t fieldId, size_t subField = FIELD_NONE);
     virtual QString getFieldName(size_t fieldId);
     virtual Executable::addr_type containsAddrType(size_t fieldId, size_t subField = FIELD_NONE);
+
+    virtual QString translateFieldContent(size_t fieldId);
 private:
     IMAGE_FILE_HEADER* hdr;
 };
