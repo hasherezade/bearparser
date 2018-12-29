b78d32045d490451a140e91c52f7b5893b733853
hasherezade <hasherezade@gmail.com>
2018-12-29 22:53:04 +0100

    [REFACT] Moved main headers to the folder "include"

---
+++ parser/include/bearparser.h
@@ -0,0 +1,10 @@
+#ifndef BEARPARSER_H
+#define BEARPARSER_H
+
+#define BEARPARSER_VERSION "0.2.5"
+
+#include "bearparser/core.h"
+#include "bearparser/pefile.h"
+
+#endif //BEARPARSER_H
+
---
+++ parser/include/bearparser/pefile.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// win headers:
+#include "../../win_hdrs/win_types.h"
+//supported formats:
+#include "../../pe/PEFile.h"
+
