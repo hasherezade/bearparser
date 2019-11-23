f9ce0446bbd97b5f76e10be9428be91cb4c79896
hasherezade <hasherezade@gmail.com>
2019-11-23 21:30:56 +0100

    [NOBIN] Give the full path to the test content

--- test.sh
+++ test.sh
@@ -9,16 +9,18 @@ git clone https://github.com/hasherezade/bearparser_tests.git
 mv bearparser_tests test_cases
 
 BCMD_DIR=$(pwd)/build/
+TEST_CASES_DIR=$(pwd)/test_cases/
 
 cd test_cases
 chmod +x test1.sh
-./test1.sh "$BCMD_DIR" "info" "x64/QtGui4"
+./test1.sh "$BCMD_DIR" "info" $TEST_CASES_DIR/"x64/QtGui4"
 if [[ "$?" != 0 ]]; then
 	exit 1
 fi
-./test1.sh "$BCMD_DIR" "winfo0" "x64/QtGui4"
+./test1.sh "$BCMD_DIR" "winfo0" $TEST_CASES_DIR/"x64/QtGui4"
 if [[ "$?" != 0 ]]; then
 	exit 1
 fi
 echo "All passed"
 cd "$START_DIR"
+
