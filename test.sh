f055010b52d5eccad75a8213709052ba4dbd9d37
hasherezade <hasherezade@gmail.com>
2019-11-24 01:02:01 +0100

    [NOBIN] Do not remove the test direcory if already exist

--- test.sh
+++ test.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 START_DIR=$(pwd)
 SOURCE_DIR=$1
-cd "$SOURCE_DIR"
-echo "Source Dir: ""$SOURCE_DIR"
+TESTS_DIR="test_cases"
 
-rm -rf test_cases
-git clone https://github.com/hasherezade/bearparser_tests.git
-mv bearparser_tests test_cases
+echo "Source Dir: ""$SOURCE_DIR"
+cd "$SOURCE_DIR"
+
+if [ -d $TESTS_DIR ]; then
+	echo "Test directory already exits"
+else
+	git clone https://github.com/hasherezade/bearparser_tests.git
+	mv bearparser_tests $TESTS_DIR
+fi
 
 BCMD_DIR=$(pwd)/build/
-TEST_CASES_DIR=$(pwd)/test_cases/
+TEST_CASES_PATH=$(pwd)/test_cases/
 
-cd test_cases
+cd $TESTS_DIR
 chmod +x test1.sh
-./test1.sh "$BCMD_DIR" "info" $TEST_CASES_DIR/"x64/QtGui4"
+./test1.sh "$BCMD_DIR" "info" $TEST_CASES_PATH/"x64/QtGui4"
 if [[ "$?" != 0 ]]; then
 	exit 1
 fi
-./test1.sh "$BCMD_DIR" "winfo0" $TEST_CASES_DIR/"x64/QtGui4"
+./test1.sh "$BCMD_DIR" "winfo0" $TEST_CASES_PATH/"x64/QtGui4"
 if [[ "$?" != 0 ]]; then
 	exit 1
 fi
