49c8670516b5845806728e1c0319733ba0accca4
hasherezade <hasherezade@gmail.com>
2019-11-24 07:02:42 +0100

    [NOBIN] Refactored the test script: keep test list in a file

--- test.sh
+++ test.sh
@@ -16,48 +16,25 @@ fi
 BCMD_DIR=$(pwd)/build/
 FAILED=0
 
+tests_list=$SOURCE_DIR/"tests_list.txt"
+
 cd $TESTS_DIR
 chmod +x test1.sh
-./test1.sh "$BCMD_DIR" "info" "x64/QtGui4"
-if [[ "$?" != 0 ]]; then
-	FAILED=$FAILED+1
-fi
-./test1.sh "$BCMD_DIR" "winfo0" "x64/QtGui4"
-if [[ "$?" != 0 ]]; then
-	FAILED=$FAILED+1
-fi
-./test1.sh "$BCMD_DIR" "winfo1" "x64/QtGui4"
-if [[ "$?" != 0 ]]; then
-	FAILED=$FAILED+1
-fi
-./test1.sh "$BCMD_DIR" "secinfo" "x64/QtGui4"
-if [[ "$?" != 0 ]]; then
-	FAILED=$FAILED+1
-fi
-#./test1.sh "$BCMD_DIR" "secinfo" "x86/overlapping_sec"
-#if [[ "$?" != 0 ]]; then
-#	FAILED=$FAILED+1
-#fi
 
-./test1.sh "$BCMD_DIR" "info" "tinype/tiny.128/tiny"
-if [[ "$?" != 0 ]]; then
-	FAILED=$FAILED+1
-fi
-./test1.sh "$BCMD_DIR" "winfo3" "tinype/tiny.128/tiny"
-if [[ "$?" != 0 ]]; then
-	FAILED=$FAILED+1
-fi
+while IFS=' ' read -r p1 p2
+do
+	if [[ "$p2" == "" || "$p1" == "" ]]; then
+		continue;
+	fi
+	if [[ "$p1" == "#" ]]; then # allow for commenting out lines
+		continue;
+	fi
 
-./test1.sh "$BCMD_DIR" "winfo7" "x86/ghost_crackme"
-if [[ "$?" != 0 ]]; then
-	FAILED=$FAILED+1
-fi
-
-./test1.sh "$BCMD_DIR" "rstrings" "x86/ghost_crackme"
-if [[ "$?" != 0 ]]; then
-	FAILED=$FAILED+1
-fi
-
+	./test1.sh "$BCMD_DIR" "$p2" "$p1"
+	if [[ "$?" != 0 ]]; then
+		FAILED=$FAILED+1
+	fi
+done < "$tests_list"
 
 if [[ "$FAILED" == 0 ]]; then
 	echo "All passed"
