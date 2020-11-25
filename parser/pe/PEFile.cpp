f8f58837675d93db681851c83fd65c3ad5e28b0f
hasherezade <hasherezade@users.noreply.github.com>
2020-11-25 14:42:10 +0100

    [BUGFIX] RVAs between the headers and the first section are not mapped (Issue #11)

--- parser/pe/PEFile.cpp
+++ parser/pe/PEFile.cpp
@@ -321,7 +321,13 @@ offset_t PEFile::rvaToRaw(offset_t rva)
         }
         return bgnRaw + curr;
     }
-    if (rva >= this->getMappedSize(Executable::RAW)) return INVALID_ADDR;
+    if (rva >= this->getMappedSize(Executable::RAW)) {
+        return INVALID_ADDR;
+    }
+    if (rva >= this->hdrsSize()) {
+        // the address is in the cave between the headers and the first section: cannot be mapped
+        return INVALID_ADDR;
+    }
     // at this point we are sure that the address is within the raw size:
     return rva;
 }
