f5b2294b6eeab26b74157c595893fa8c31d6a947
hasherezade <hasherezade@gmail.com>
2022-09-08 07:43:42 +0200

    [FEATURE] In ExeFactory: autoinit builders

--- parser/ExeFactory.cpp
+++ parser/ExeFactory.cpp
@@ -27,6 +27,8 @@ void ExeFactory::destroy()
 ExeFactory::exe_type ExeFactory::findMatching(AbstractByteBuffer *buf)
 {
     if (!buf) return NONE;
+    
+    ExeFactory::init(); //ensue that the builders are initialized
 
     std::map<exe_type, ExeBuilder*>::iterator itr;
     for (itr = builders.begin(); itr != builders.end(); itr++) {
@@ -43,16 +45,21 @@ ExeFactory::exe_type ExeFactory::findMatching(AbstractByteBuffer *buf)
 
 Executable* ExeFactory::build(AbstractByteBuffer *buf, exe_type type)
 {
-    if (builders.find(type) == builders.end()) return NULL;
+    ExeFactory::init(); //ensue that the builders are initialized
 
+    if (builders.find(type) == builders.end()) {
+        return NULL;
+    }
     ExeBuilder* builder = builders[type];
-    if (builder == NULL) return NULL;
+    if (!builder) return NULL;
 
     return builder->build(buf);
 }
 
 QString ExeFactory::getTypeName(exe_type type)
 {
+    ExeFactory::init(); //ensue that the builders are initialized
+
     if (builders.find(type) == builders.end()) return "Not supported";
 
     ExeBuilder* builder = builders[type];
