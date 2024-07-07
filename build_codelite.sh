72f6762313b19c8637bcd3ea1c635a59e4dc48f5
hasherezade <hasherezade@gmail.com>
2024-07-07 22:22:17 +0200

    [NOBIN] Updated autobuilder script

--- build_codelite.sh
+++ build_codelite.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
-echo "Trying to autobuild bearparser..."
+
+echo "Trying to build bearparser..."
 
 #QT check
 
-QT_VER=`qmake -v`
-str=$QT_VER
-substr="Qt version 5"
+QT_VER=$(qmake -v)
+str="$QT_VER"
+substr="Qt version 6"
 
 echo $QT_VER
 if [[ $str == *"$substr"* ]]; then
-    echo "[+] Qt5 found!"
+    echo "[+] Qt6 found!"
 else
-    str2=`whereis qt5`
-    substr2="/qt5"
+    str2=$(whereis qt6)
+    substr2="/qt6"
     if [[ $str2 == *"$substr2"* ]]; then
-        echo "[+] Qt5 found!"
+        echo "[+] Qt6 found!"
     else
-        echo "Install Qt5 SDK first"
+        echo "Install Qt6 SDK first"
         exit -1
     fi
 fi
 
-CMAKE_VER=`cmake --version`
+CMAKE_VER=$(cmake --version)
 CMAKEV="cmake version"
 if echo "$CMAKE_VER" | grep -q "$CMAKEV"; then
     echo "[+] CMake found!"
@@ -30,11 +31,12 @@ else
     echo "Install cmake first"
     exit -1
 fi
-
-BUILD_DIR=build
-
-mkdir $BUILD_DIR
+echo $CMAKE_VER
+mkdir build_qt6
 echo "[+] build directory created"
-cd $BUILD_DIR
-cmake -G "CodeLite - Unix Makefiles" -DUSE_QT4=OFF -DCMAKE_INSTALL_PREFIX:PATH=$(pwd) ..
+cd build_qt6
+cmake -G"CodeLite - Unix Makefiles" -DCMAKE_INSTALL_PREFIX:PATH=$(pwd) ..
 cmake --build . --target install
+make
+cd ..
+
